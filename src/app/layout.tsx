a79e888595e139385a03921e341845bdefd01b7a
KaitosAgency <kaitosagency@gmail.com>
2026-03-24 11:58:33 +0100

    gsc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,9 +104,9 @@ export default async function RootLayout({
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
-              strategy="afterInteractive"
+              strategy="beforeInteractive"
             />
-            <Script id="ga4-init" strategy="afterInteractive">
+            <Script id="ga4-init" strategy="beforeInteractive">
               {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${gaMeasurementId}');`}
             </Script>
           </>
